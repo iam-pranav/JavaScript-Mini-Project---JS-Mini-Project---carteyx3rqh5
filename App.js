11dfdcaa818a8a4f9dfe144619ce9d0e340a1879
iam-pranav <41188797+iam-pranav@users.noreply.github.com>
2022-09-29 21:49:56 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,10 @@
+let str = '';
+const screen = document.getElementById("io");
 
+function entry(s){
+  str += s;
+  screen.value = str;
+}
+function result(){
+  screen.value = eval(screen.value);
+}
